cf36e03b842eb93d43d0e1062e18683520bd9c12
Phyo Yarzar <phyoyazar592@gmail.com>
2024-04-02 22:31:07 +0630

    implememnt panic

--- app/services/sales-api/handlers/v1/testgrp/testgrp.go
+++ app/services/sales-api/handlers/v1/testgrp/testgrp.go
@@ -13,12 +13,11 @@ import (
 func Test(ctx context.Context,w http.ResponseWriter, r *http.Request) error {
 	if n := rand.Intn(100); n%2 == 0 {
 		return v1.NewRequestError(errors.New("TRUSTED ERROR"), http.StatusBadRequest)
+		// panic("OHH NOO PANIC")
 	 }
 
 	// 1. Validate the data
 	// 2. Call into the business layer
-	// 3. Return errors
-	// 4. Handle OK response
 
 	status := struct{
 		Status string `json:"status"`
