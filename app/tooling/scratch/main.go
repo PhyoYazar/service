88f6444de905b4970273cf1e9cb55467432a8482
Phyo Yarzar <phyoyazar592@gmail.com>
2024-04-04 12:39:58 +0630

    implement opa authentication & authorization

--- app/tooling/scratch/main.go
+++ app/tooling/scratch/main.go
@@ -1,16 +1,21 @@
 package main
 
 import (
+	"bytes"
+	"context"
 	"crypto/rand"
 	"crypto/rsa"
 	"crypto/x509"
+	_ "embed"
 	"encoding/pem"
+	"errors"
 	"fmt"
 	"log"
 	"os"
 	"time"
 
 	"github.com/golang-jwt/jwt/v4"
+	"github.com/open-policy-agent/opa/rego"
 )
 
 func main() {
@@ -112,8 +117,115 @@ func genToken() error {
 	fmt.Println("TOKEN VALIDATED")
 
 	// -------------------------------------------------------------------------
+ 
+	var b bytes.Buffer
+	if err := pem.Encode(&b, &publicBlock); err != nil {
+		return fmt.Errorf("encoding to public file: %w", err)
+	}
+
+	ctx := context.Background()
+	if err := opaPolicyEvaluationAuthen(ctx, b.String(), str, clm.Issuer); err != nil {
+		return fmt.Errorf("OPS authentication failed: %w", err)
+	}
+
+	fmt.Println("TOKEN VALIDATED BY OPA")
+
+	// -------------------------------------------------------------------------
+
+	if err := opaPolicyEvaluationAuthor(ctx); err != nil {
+		return fmt.Errorf("OPS authorization failed: %w", err)
+	}
+
+	fmt.Println("AUTH VALIDATED BY OPA")
+
+	// ------------------------------------------------------------------------- 
 
 	fmt.Printf("\n%#v\n", clm)
+
+	return nil
+}
+
+
+// Core OPA policies.
+var (
+	//go:embed rego/authentication.rego
+	opaAuthentication string
+
+	//go:embed rego/authorization.rego
+	opaAuthorization string
+)
+
+
+func opaPolicyEvaluationAuthor(ctx context.Context) error {
+	const rule = "ruleAdminOnly"
+	const opaPackage string = "yarzar.rego"
+
+	query := fmt.Sprintf("x = data.%s.%s", opaPackage, rule)
+
+	q, err := rego.New(
+		rego.Query(query),
+		rego.Module("policy.rego", opaAuthorization),
+	).PrepareForEval(ctx)
+	if err != nil {
+		return err
+	}
+
+	input := map[string]any{
+		"Roles":   []string{"ADMIN"},
+		"Subject": "1234567",
+		"UserID":  "1234567",
+	}
+
+	results, err := q.Eval(ctx, rego.EvalInput(input))
+	if err != nil {
+		return fmt.Errorf("query: %w", err)
+	}
+
+	if len(results) == 0 {
+		return errors.New("no results")
+	}
+
+	result, ok := results[0].Bindings["x"].(bool)
+	if !ok || !result {
+		return fmt.Errorf("bindings results[%v] ok[%v]", results, ok)
+	}
+
+	return nil
+}
+
+func opaPolicyEvaluationAuthen(ctx context.Context, pem string, tokenString string, issuer string) error {
+	const rule = "auth"
+	const opaPackage string = "yarzar.rego"
+
+	query := fmt.Sprintf("x = data.%s.%s", opaPackage, rule)
+
+	q, err := rego.New(
+		rego.Query(query),
+		rego.Module("policy.rego", opaAuthentication),
+	).PrepareForEval(ctx)
+	if err != nil {
+		return err
+	}
+
+	input := map[string]any{
+		"Key":   pem,
+		"Token": tokenString,
+		"ISS":   issuer,
+	}
+
+	results, err := q.Eval(ctx, rego.EvalInput(input))
+	if err != nil {
+		return fmt.Errorf("query: %w", err)
+	}
+
+	if len(results) == 0 {
+		return errors.New("no results")
+	}
+
+	result, ok := results[0].Bindings["x"].(bool)
+	if !ok || !result {
+		return fmt.Errorf("bindings results[%v] ok[%v]", results, ok)
+	}
 
 	return nil
 }
